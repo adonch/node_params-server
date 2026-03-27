d7fe29aa6284decd31c36091d21db8429f2b72e2
adonch <donch.a080@gmail.com>
2026-03-27 20:40:47 +0200

    feat: implement HTTP server with request handling
    - Add createServer function to handle incoming requests.
    - Parse URL and extract pathname and search parameters.
    - Return JSON response for valid requests and 404 for not found.

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,31 @@
 /* eslint-disable no-console */
 'use strict';
 
+const http = require('http');
+
 function createServer() {
   /* Write your code here */
   // Return instance of http.Server class
+  return http.createServer((req, res) => {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const pathname = url.pathname.slice(1).split('/');
+
+    const searchParams = url.searchParams;
+
+    if (pathname.length > 0 || searchParams.size > 0) {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+
+      res.end(
+        JSON.stringify({
+          parts: pathname,
+          query: Object.fromEntries(searchParams),
+        }),
+      );
+    } else {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Not Found');
+    }
+  });
 }
 
 module.exports = {
